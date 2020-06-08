d09e0f11cd61e55d519e96307c49594926bc8955
Zhaoliqun <1440169549@qq.com>
2020-06-08 17:11:26 +0800

    匹配字母表功能

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="list">
-      <div class="item" v-for="(item,key) of cities" :key='key' @click='handleClick'>{{key}}</div>
+      <div class="item" v-for="item of letters" :key='item'
+      :ref='item'
+      @click='handleClick'
+      @touchstart='handleTouchstart'
+      @touchmove='handleTouchmove'
+      @touchend='handleTouchend'>
+      {{item}}
+      </div>
     </div>
 </template>
 
@@ -10,9 +17,48 @@ export default {
   props: {
     cities: Object
   },
+  data () {
+    return {
+      touchStatus: false,
+      timer: null,
+      startY: 0
+    }
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
+  },
   methods: {
     handleClick (e) {
       this.$emit('change', e.target.innerText)
+    },
+    handleTouchstart () {
+      this.touchStatus = true
+    },
+    handleTouchmove (e) {
+      if (this.touchStatus) {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          const touchY = e.touches[0].clientY - 88.4
+          const move = Math.floor((touchY - this.startY) / 20)
+          if (move >= 0 && move < this.letters.length) {
+            this.$emit('change', this.letters[move])
+          }
+        }, 16)
+      }
+    },
+    handleTouchend () {
+      this.touchStatus = false
     }
   }
 }
@@ -24,7 +70,7 @@ export default {
   flex-direction: column
   justify-content: center
   position: absolute
-  top: 1.78rem
+  top: 0
   right: 0
   bottom: 0
   width: .4rem
