114c3b237114b8dc9ac2bac1ce3a4cc77bf7b30b
Zhaoliqun <1440169549@qq.com>
2020-06-11 10:38:40 +0800

    add list

---
+++ src/pages/detail/components/detail-list.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="list">
+    <div class="item"
+    v-for="(item,index) of list"
+    :key="index"
+    >
+      <div class="item-title">
+        {{item.title}}
+        <div v-if="item.children">
+          <detail-list :list='item.children'></detail-list>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .item-title
+    padding: 0 .2rem
+    line-height: .8rem
+</style>
